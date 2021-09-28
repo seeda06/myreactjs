7ba44c57db42d6a0c0d597fdbb67d8db36e6435b
seeda06 <86487372+seeda06@users.noreply.github.com>
2021-09-28 17:28:08 +0700

    myreactjs

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import MyFirstPage from './MyFirstPage';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <MyFirstPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
